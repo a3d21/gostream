ed2b8d82806ea79999a1a4159ab2a2e3183b5025
a3d21 <a3d21@outlook.com>
2021-11-19 15:11:59 +0800

    refactor
    Change-Id: I09738027d83cfd024e69d3b983d14acaa77cecf8

--- gopark/slice2map.go
+++ gopark/slice2map.go
@@ -5,24 +5,24 @@ import (
 )
 
 var (
-	boolTyp = reflect.TypeOf(true)
-	boolVal = reflect.ValueOf(true)
+	trueTyp = reflect.TypeOf(true)
+	trueVal = reflect.ValueOf(true)
 )
 
-// Slice2Map 装slice[T]转成成map[T]bool，且值为true
+// Slice2Map 将[]T转成map[T]bool，且值为true
 func Slice2Map(vs interface{}) interface{} {
 	t := reflect.TypeOf(vs)
 	if t.Kind() != reflect.Slice {
 		panic("typ should be slice")
 	}
 
-	mtyp := reflect.MapOf(t.Elem(), boolTyp)
+	mtyp := reflect.MapOf(t.Elem(), trueTyp)
 	mval := reflect.Indirect(reflect.MakeMap(mtyp))
 
 	v := reflect.ValueOf(vs)
 	vlen := v.Len()
 	for i := 0; i < vlen; i++ {
-		mval.SetMapIndex(v.Index(i), boolVal)
+		mval.SetMapIndex(v.Index(i), trueVal)
 	}
 
 	return mval.Interface()
